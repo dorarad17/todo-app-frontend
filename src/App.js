c0770871414ccd9a1ac75a733c6fd4c68c9e182f
Dora <dorarad17@gmail.com>
2019-12-26 13:51:03 +0100

    added title component and working

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Nav from "./Nav";
+import Title from "./Title";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<Title />
 				<Nav />
 			</div>
 		);
---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Title extends React.Component {
+	render() {
+		return (
+			<head>
+				<title>My To Do List</title>
+			</head>
+		);
+	}
+}
+
+export default Title;
