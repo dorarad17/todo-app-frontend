f45b322e7dea5faa8a6b0dbf1677b03957368af5
Dora <dorarad17@gmail.com>
2019-12-26 15:28:54 +0100

    moved components into components folder nd updated imports

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Nav from "./Nav";
-import Title from "./Title";
-import NewTask from "./NewTask";
-import Background from "./Background";
-import Total from "./Total";
-import Tasks from "./Tasks";
+import Nav from "./components/Nav";
+import Title from "./components/Title";
+import NewTask from "./components/NewTask";
+import Background from "./components/Background";
+import Total from "./components/Total";
+import Tasks from "./components/Tasks";
 
 class App extends React.Component {
 	render() {
