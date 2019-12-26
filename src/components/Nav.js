e4438fc071ffc7f3404e28989bc495a22898662d
Dora <dorarad17@gmail.com>
2019-12-26 15:41:55 +0100

    removed unnecessary divs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,14 +3,12 @@ import React from "react";
 class Nav extends React.Component {
 	render() {
 		return (
-			<div>
-				<div className="container">
-					<nav className="navbar fixed-top navbar-expand-md">
-						<a className="navbar-brand" href="">
-							<h1>To Do List</h1>
-						</a>
-					</nav>
-				</div>
+			<div className="container">
+				<nav className="navbar fixed-top navbar-expand-md">
+					<a className="navbar-brand" href="">
+						<h1>To Do List</h1>
+					</a>
+				</nav>
 			</div>
 		);
 	}
