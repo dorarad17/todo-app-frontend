488298884af147fc781da0795cf48608b2d53735
dorarad17 <dorarad17@gmail.com>
2020-03-08 12:18:43 +0000

    Updates

---
+++ precache-manifest.bea55b967ce4e188be86abd30721e0ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8ea1c43a0139573f2e2f3d2552881a",
+    "url": "/todo-app-frontend/index.html"
+  },
+  {
+    "revision": "4a0bf26ca043a8e85500",
+    "url": "/todo-app-frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50a4fe1fca5a397d0349",
+    "url": "/todo-app-frontend/static/js/2.91637718.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-app-frontend/static/js/2.91637718.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a0bf26ca043a8e85500",
+    "url": "/todo-app-frontend/static/js/main.4700b70c.chunk.js"
+  },
+  {
+    "revision": "f6b929557a38fdd4f4fc",
+    "url": "/todo-app-frontend/static/js/runtime-main.ea3b24bc.js"
+  }
+]);
